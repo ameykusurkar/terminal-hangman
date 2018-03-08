0cf8a9036de6468904641f90e2f938c2b36e1414
Amey Kusurkar <amey1000@gmail.com>
2018-03-08 17:13:03 +0000

    Hangman implementation

---
+++ hangman.rb
@@ -0,0 +1,58 @@
+require "set"
+
+class Hangman
+  def initialize(word="hello", lives=5)
+    @word = word
+    @lives = lives
+    @guessed_letters = Set.new
+  end
+
+  def guess(char)
+    return if @guessed_letters.include?(char)
+
+    @guessed_letters << char
+    @lives -= 1 unless @word.chars.include?(char)
+  end
+
+  def finished?
+    @lives.zero? ||
+      @word.chars.all? { |c| @guessed_letters.include?(c) }
+  end
+
+  def show
+    @word.chars.map { |c| @guessed_letters.include?(c) ? c : "_" }.join(" ")
+  end
+
+  attr_reader :word, :lives, :guessed_letters
+end
+
+def get_random_word
+  File.readlines("/usr/share/dict/words").sample.chomp
+end
+
+game = Hangman.new(get_random_word, 8)
+
+until game.finished?
+  system("clear")
+  puts
+  puts game.show
+  puts
+  puts "#{game.word.length}-letter word. #{game.lives} live(s) left."
+  unless game.guessed_letters.empty?
+    puts "So far you have tried:"
+    puts game.guessed_letters.to_a.sort.join(", ")
+  end
+  puts "Pick a letter:"
+
+  attempt = gets
+  next if attempt.empty? || attempt == "\n" 
+
+  game.guess(attempt[0])
+end
+
+if game.lives.zero?
+  puts "It looks like you lost. The word was \"#{game.word}\"."
+else
+  puts "\"#{game.word}\""
+  puts "Congratulations! You got it!"
+end
